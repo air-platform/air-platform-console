9ce55ba29fc2a624dccaaddd8288f5a8a49177f7
kongxiangwen <xw.kong@hnair.com>
2017-03-29 13:08:55 +0800

    modify k8s addresss

--- gulp/run.js
+++ gulp/run.js
@@ -41,7 +41,7 @@ function browserSyncInit(baseDir, browser) {
   var proxy_main = proxyMiddleware('/iot/api/v1/**', {target: 'http://localhost:8999', changeOrigin: true});
   var proxy_ua = proxyMiddleware('/cp-ua/v1/**', {target: 'http://172.16.5.240:8072', changeOrigin: true});
   var proxy_gogs = proxyMiddleware('/appMng/v1/**', {target: 'http://172.16.5.119:8088', changeOrigin: true});
-  var proxy_k8s = proxyMiddleware('/v1/**', {target: 'http://172.16.5.245:8080', changeOrigin: true});
+  var proxy_k8s = proxyMiddleware('/k8s-middleware/v1/**', {target: 'http://172.16.5.245:8080', changeOrigin: true});
   var proxy_cloudservice = proxyMiddleware('/cp/v1/**', {target: 'http://172.16.5.240:8073', changeOrigin: true});
    server.middleware = [proxy_main, proxy_ua, proxy_gogs, proxy_k8s,proxy_cloudservice];//proxyMiddleware('/users', {target: 'http://jsonplaceholder.typicode.com', changeOrigin: true});
   //http://127.0.0.1:8080/iot/api/v1/
--- src/app/app.constants.js
+++ src/app/app.constants.js
@@ -37,7 +37,7 @@
                         {"appsPath":"/appMng/v1/apps",
                          "imgsPath":"/appMng/v1/images",
                             "imgsAppPath":"/appMng/v1/apps",
-                            "depPath":"/v1"
+                            "depPath":"//k8s-middleware/v1"
                         },
                     "event":
                         {"eventPath":"/iot/api/v1/events"},
