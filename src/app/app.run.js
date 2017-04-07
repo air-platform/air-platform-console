d78c7d4bcac2324bca65cdcfe8168808d965b795
__无邪_ <fqah@qq.com>
2017-04-07 10:37:09 +0800

    f

--- src/app/app.run.js
+++ src/app/app.run.js
@@ -13,12 +13,8 @@
     angular.module('airs').run(runBlock);
 
     /** @ngInject */
-    function runBlock($log,$rootScope) {
-        $log.debug('IoT app started.');
-        $rootScope.conversationIndex = 1; //控制training 弹出框那个显示左边框和背景颜色
-        $rootScope.selectParmeterIsShow = false; //控制 点击intent 边框弹出的显示隐藏
-        $rootScope.parmeterActiveIsShow = false; // 控制 输入框选中后的时间显示隐藏
-        $rootScope.trainingList = []; //数据列表
+    function runBlock($log) {
+        $log.debug('Hold on, Air Start Fly.');
     }
 
 
