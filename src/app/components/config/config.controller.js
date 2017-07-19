c33208d5ff97eecba0976ec4828779d68ddb1e1e
kongxiangwen <xw.kong@hnair.com>
2017-07-19 13:29:30 +0800

    add platform config tab page

--- src/app/components/config/config.controller.js
+++ src/app/components/config/config.controller.js
@@ -31,32 +31,238 @@
         vm.backAction = backAction;
         vm.userInfo = {};
 
+        vm.ruleTitle = {
+            'order_finished_air_training':'航校课程订单完成获得积分数',
+            'order_finished_air_taxi':'空中出租订单完成获得积分数',
+            'order_finished_air_tour':'观光飞行订单完成获得积分数',
+            'account_registration':'注册时送积分数',
+            'daily_signin_20':'连续签到20天获得积分数',
+            'daily_signin_5':'连续签到5天获得积分数',
+            'daily_signin_15':'连续签到15天获得积分数',
+            'daily_signin_25':'连续签到25天获得积分数',
+            'order_first_price_off':'首单立减钱数（人民币：元）',
+            'order_finished_air_trans':'通勤航旅订单完成获得积分数',
+            'daily_signin_1':'连续签到1天获得积分数',
+            'order_finished_air_jet':'包机订单完成获得积分数',
 
+        }
 
-        vm.subPath = 'airclasses';
-        vm.reqPath =  constdata.api.tenant.basePath;
+        vm.subPath = 'client-managers';
+        vm.subPathPointBasic = 'point/basics';
+        vm.subPathPointRule = 'point/rules';
+        vm.reqPath =  constdata.api.tenant.platPath+'/settings';
 
         vm.isAdmin = false;
         vm.userInfo = StorageService.get('iot.hnair.cloud.information');
         if(vm.userInfo.role != 'tenant'){
-            vm.reqPath = constdata.api.admin.platPath;
+            vm.reqPath = constdata.api.admin.platPath+'/settings';
 
             vm.isAdmin = true;
         }
 
+        vm.isEdit = false;
+        vm.editTitle = '编辑';
+        vm.isEditPointBasic = false;
+        vm.editTitlePointBasic = '编辑';
+
+        vm.isEditPointRule = false;
+        vm.editTitlePointRule = '编辑';
+
+        vm.tabItem =
+            [{
+                title:'平台客户经理',
+                active:true,
+                id:1
+
+            },
+            {
+                title:'积分规则',
+                active:false,
+                    id:2
+            }/*,
+            {
+                title:'其他',
+                active:false,
+                    id:3
+            }*/
+            ];
 
 
-        function getDatas() {
+        vm.sel = function(oper){
+            console.log('hi' + oper);
+            for(var i = 0; i < vm.tabItem.length; i ++){
+                if(vm.tabItem[i].id == oper){
+                    vm.tabItem[i].active = true;
+                }else{
+                    vm.tabItem[i].active = false;
+                }
+            }
+
+            if(oper == 1){
+                getPlatMgr();
+            }else if(oper == 2){
+                getPointBasic();
+                getPointRule();
+            }
+
+        }
+        vm.sel(1);
+
+
+        function getPlatMgr() {
 
             NetworkService.get(vm.reqPath + '/' + vm.subPath,{page:vm.pageCurrent},function (response) {
-                vm.items = response.data.content;
-                console.log(response.data);
-                vm.displayedCollection = [].concat(vm.items);
-                updatePagination(response.data);
+               // vm.items = response.data.content;
+                //vm.platMgr = [{person:'aa',email:'aa@qq.com', mobile:'13223232321'},{person:'bb',email:'bb@qq.com', mobile:'13323232322'}];
+                vm.platMgr = response.data;
+               console.log(vm.platMgr);
+               // vm.displayedCollection = [].concat(vm.items);
+                //updatePagination(response.data);
             },function (response) {
                 toastr.error(i18n.t('u.GET_DATA_FAILED') + response.status + ' ' + response.statusText);
             });
         }
+
+
+        function addPlatMgr() {
+            NetworkService.put(vm.reqPath + '/' + vm.subPath,vm.platMgr,function (response) {
+                toastr.success(i18n.t('u.OPERATE_SUC'));
+                getPlatMgr();
+            },function (response) {
+                vm.authError = response.statusText + '(' + response.status + ')';
+                console.log(vm.authError);
+                toastr.error(i18n.t('u.OPERATE_FAILED') + vm.authError);
+            });
+        }
+
+
+
+
+
+        function getPointBasic() {
+
+            NetworkService.get(vm.reqPath + '/' + vm.subPathPointBasic,{page:vm.pageCurrent},function (response) {
+                // vm.items = response.data.content;
+                //vm.platMgr = [{person:'aa',email:'aa@qq.com', mobile:'13223232321'},{person:'bb',email:'bb@qq.com', mobile:'13323232322'}];
+                vm.pointBasic = response.data;
+                console.log(vm.pointBasic);
+                // vm.displayedCollection = [].concat(vm.items);
+                //updatePagination(response.data);
+            },function (response) {
+                toastr.error(i18n.t('u.GET_DATA_FAILED') + response.status + ' ' + response.statusText);
+            });
+        }
+
+
+        function addPointBasic() {
+            vm.pointBasic.exchangeRate = parseInt(vm.pointBasic.exchangeRate);
+            vm.pointBasic.exchangePercent = parseInt(vm.pointBasic.exchangePercent);
+            NetworkService.put(vm.reqPath + '/' + vm.subPathPointBasic,vm.pointBasic,function (response) {
+                toastr.success(i18n.t('u.OPERATE_SUC'));
+                getPointBasic();
+            },function (response) {
+                vm.authError = response.statusText + '(' + response.status + ')';
+                console.log(vm.authError);
+                toastr.error(i18n.t('u.OPERATE_FAILED') + vm.authError);
+            });
+        }
+
+
+
+
+        function getPointRule() {
+
+            NetworkService.get(vm.reqPath + '/' + vm.subPathPointRule,{page:vm.pageCurrent},function (response) {
+                // vm.items = response.data.content;
+                //vm.platMgr = [{person:'aa',email:'aa@qq.com', mobile:'13223232321'},{person:'bb',email:'bb@qq.com', mobile:'13323232322'}];
+                vm.pointRule = response.data;
+                console.log(vm.pointRule);
+            },function (response) {
+                toastr.error(i18n.t('u.GET_DATA_FAILED') + response.status + ' ' + response.statusText);
+            });
+        }
+
+
+        function addPointRule() {
+            NetworkService.put(vm.reqPath + '/' + vm.subPathPointRule,vm.pointRule,function (response) {
+                toastr.success(i18n.t('u.OPERATE_SUC'));
+                getPointRule();
+            },function (response) {
+                vm.authError = response.statusText + '(' + response.status + ')';
+                console.log(vm.authError);
+                toastr.error(i18n.t('u.OPERATE_FAILED') + vm.authError);
+            });
+        }
+
+
+
+
+        vm.editOper = function(operId){
+            if(operId == 11) {
+                if (!vm.isEdit) {
+                    vm.isEdit = true;
+                    vm.editTitle = '保存';
+                } else {
+                    vm.isEdit = false;
+                    vm.editTitle = '编辑';
+                    addPlatMgr();
+                }
+            }else if(operId == 12){
+                if (!vm.isEditPointBasic) {
+                    vm.isEditPointBasic = true;
+                    vm.editTitlePointBasic = '保存';
+                } else {
+                    vm.isEditPointBasic = false;
+                    vm.editTitlePointBasic = '编辑';
+                    addPointBasic();
+                }
+            }else if(operId == 13){
+                if (!vm.isEditPointRule) {
+                    vm.isEditPointRule = true;
+                    vm.editTitlePointRule = '保存';
+                } else {
+                    vm.isEditPointRule = false;
+                    vm.editTitlePointRule = '编辑';
+                    addPointRule();
+                }
+            }
+
+        }
+        vm.cancelOper = function(operId){
+
+            if(operId == 11) {
+                vm.isEdit = false;
+                vm.editTitle = '编辑';
+                getPlatMgr();
+            }else if(operId == 12){
+                vm.isEditPointBasic = false;
+                vm.editTitlePointBasic = '编辑';
+                getPointBasic();
+
+            }else if(operId == 13){
+                vm.isEditPointRule = false;
+                vm.editTitlePointRule = '编辑';
+                getPointRule();
+            }
+
+
+        }
+
+
+        vm.addNewClientManager = function() {
+
+            vm.platMgr.push({
+                person:'',
+                mobile:'',
+                email:''
+            })
+        }
+
+        vm.removeClientManager = function(item) {
+            var index = vm.platMgr.indexOf(item);
+            vm.platMgr.splice(index, 1);
+        }
+
 
 
         function goAddItem() {
@@ -159,8 +365,8 @@
 
         }
 
-        getDatas();
 
+        //addItem();
 
         //Model
 
